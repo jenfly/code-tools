3b70e344b8572aee95199b8911991650a1f5a685
Jennifer Walker <jenfly@gmail.com>
2015-10-03 17:22:02 -0700

    Added command line script functionality

--- code_tools.py
+++ code_tools.py
@@ -1,23 +1,29 @@
-"""Handy utilities for .py files."""
+#!/usr/bin/python
 
+"""Handy utilities for .py files.
 
-def _function_name(line):
-    """Return the name of function defined in line."""
-    if not line.startswith('def '):
-        return None
+Import as a module in Python or run as a command line script.
+"""
 
-    name = line.split('def ')[1]
-    name = name.split('(')[0]
-    return name
+import sys
 
-def function_list(filename):
+def funclist(filename):
     """Return and display a list of functions from a .py file."""
+
+    def function_name(line):
+        """Return the name of function defined in line."""
+        if not line.startswith('def '):
+            return None
+
+        name = line.split('def ')[1]
+        name = name.split('(')[0]
+        return name
 
     # Extract all function names
     with open(filename, 'rU') as f:
         names = []
         for line in f:
-            name = _function_name(line)
+            name = function_name(line)
             if name is not None:
                 names.append(name)
 
@@ -27,3 +33,20 @@ def function_list(filename):
         print(pad + name + ',')
 
     return names
+
+def main():
+    args = sys.argv[1:]
+    if not args:
+        raise RuntimeError('Usage: code_tools funcname [args]')
+
+    # Get function name from first command line argument and call function
+    # with remaining arguments as input
+    funcname = args.pop(0)
+    if funcname not in globals().keys():
+        raise RuntimeError('Function ' + funcname + ' not found.')
+    func = globals()[funcname]
+    func(*args)
+
+
+if __name__ == "__main__":
+    main()
